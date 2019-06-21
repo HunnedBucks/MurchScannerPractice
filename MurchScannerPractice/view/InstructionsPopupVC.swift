afcb6af07d0287ff4b724a1739a86a7db1cd3049
HunterBuxton <huntercbuxton@icloud.com>
2019-06-21 11:02:58 -0600

    committed a new file regarding hadoop and the measurements data model

---
+++ MurchScannerPractice/view/InstructionsPopupVC.swift
@@ -0,0 +1,30 @@
+//
+//  InstructionsPopupVC.swift
+//  MurchScannerPractice
+//
+//  Created by Hunter Buxton on 6/21/19.
+//  Copyright © 2019 Hunter Buxton. All rights reserved.
+//
+
+import UIKit
+
+class InstructionsPopupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ MurchScannerPractice/view/MachineLearningModel.swift
@@ -0,0 +1,15 @@
+//
+//  MachineLearningModel.swift
+//  MurchScannerPractice
+//
+//  Created by Hunter Buxton on 6/21/19.
+//  Copyright © 2019 Hunter Buxton. All rights reserved.
+//
+
+import Foundation
+
+/* soime hadoop stuff here */
+
+struct MeasurementsForShirt {
+    var sleeveLength: Double 
+}
