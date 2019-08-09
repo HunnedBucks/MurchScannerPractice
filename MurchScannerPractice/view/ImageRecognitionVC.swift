4f273ed3bcc6b19363e34cff286766b1c30f3df5
HunterBuxton <huntercbuxton@icloud.com>
2019-08-09 13:48:12 -0600

    added a .gitignore file with *.mlmodel inside

--- MurchScannerPractice/view/ImageRecognitionVC.swift
+++ MurchScannerPractice/view/ImageRecognitionVC.swift
@@ -130,7 +130,6 @@ extension ImageRecognitionVC: AVCaptureVideoDataOutputSampleBufferDelegate {
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
         guard let model = try? VNCoreMLModel(for: Resnet50().model ) else { return }
         let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
-           // print(finishedReq.results)
             guard let results = finishedReq.results as? [VNClassificationObservation] else { return }
             guard let firstObservation = results.first else { return }
             print(firstObservation.identifier, firstObservation.confidence)
